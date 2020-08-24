594a47ed0e91f7bbb152b30adbdad808506cce6b
Sasarino MARi <develop.mari@gmail.com>
2020-08-24 10:50:46 +0900

    프로그램 포커스 판별 코드 추가

--- SigotoTimer/MainWindow.xaml.cs
+++ SigotoTimer/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,6 +25,49 @@ namespace SigotoTimer
         public MainWindow()
         {
             InitializeComponent();
+            InitializeConsole();
+            startParallelThread();
+        }
+
+        private void InitializeConsole()
+        {
+            #if DEBUG
+            ConsoleManager.Init();
+            ConsoleManager.Show();
+            #endif
+        }
+
+        private void startParallelThread()
+        {
+            Task.Factory.StartNew(delegate
+            {
+                var client = new Schadenfreude();
+                var procName = "devenv";
+                while (true)
+                {
+                    bool activated = false;
+                    var procs = Process.GetProcessesByName(procName);
+                    foreach (var proc in procs)
+                    {
+                        if(client.ApplicationIsActivated(proc.Id))
+                        {
+                            activated = true;
+                            break;
+                        }
+                    }
+
+                    if(activated)
+                    {
+                        Console.WriteLine("Activated.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("De-Activated.");
+                    }
+
+                    Thread.Sleep(1000);
+                }
+            });
         }
     }
 }
